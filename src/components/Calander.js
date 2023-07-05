97edcd98be4c05a0fece549e9c9b2430681f19f9
Aman Sharma <droj.co@gmail.com>
2023-07-05 13:23:54 +0530

    added select month year

--- src/components/Calander.js
+++ src/components/Calander.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Calander.css"
 function Calander({date}) {
-    const currentYear = date.getFullYear();
-    const currentMonth = date.getMonth();
+    const [mydate,setmydate]=useState(date);
+    const currentYear = mydate.getFullYear();
+    const currentMonth = mydate.getMonth();
     const numberofdays=new Date(currentYear, currentMonth, 0).getDate()
     const day1=new Date(currentYear,currentMonth,1)
     const day1OfWeek = day1.getDay();
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const currentMonthName = months[date.getMonth()];
+    const currentMonthName = months[mydate.getMonth()];
     const currentDate = new Date();
     const dayOfMonth = currentDate.getDate();
     var arr=[];
+    const years=[];
+    for(let i=1995;i<2030;i++){
+        years.push(i);
+    }
     for (let i=0;i<day1OfWeek;i++){
 arr.push(
     {
@@ -26,14 +31,43 @@ for(let i=0;i<numberofdays;i++){
         num:(i+1).toString()
     })
 }
+
+function handlechange(e){
+setmydate( new Date(mydate.getFullYear(), e.target.value, mydate.getDay()))
+console.log(currentDate.getMonth())
+console.log(mydate.getMonth())
+}
+function handlechange2(e){
+setmydate( new Date(e.target.value, mydate.getMonth(), mydate.getDay()))
+console.log(currentDate.getMonth())
+console.log(mydate.getMonth())
+}
+
   return (
     <div>
         <h1 className='heading'>Calander</h1>
-        <h2 className='monthyear'>{currentMonthName} {currentYear}</h2>
+        <h2 className='monthyear'>{currentMonthName} {mydate.getFullYear()}</h2>
+        <label htmlFor="months"> chose month</label>
+        <select name="months" onChange={handlechange}>
+            {months.map(function(mo){
+                return(
+                    <option value={months.indexOf(mo)}>{mo}</option>
+                )
+            })}
+        </select>
+        <select name="years" onChange={handlechange2}>
+            {years.map(function(year){
+                return(
+                    <option value={year}>{year}</option>
+                )
+            })}
+        </select>
+       
         <ul>
-
+            <p>mydate-{mydate.getMonth()} currentdate-{currentDate.getMonth()}</p>
         {arr.map(function(arr){
-            if(arr.num.toString()===dayOfMonth.toString()){
+            
+            if(arr.num.toString()===dayOfMonth.toString() && mydate.getMonth()===currentDate.getMonth()-1){
                 console.log("here");
                 return(
                     <li style={{ borderColor: 'red' }}>
